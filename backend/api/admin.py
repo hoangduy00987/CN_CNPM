7318bcd211207c74c5a1d484eaf4e8845313557d
khuongduy-hoang <hoangduy00987@gmail.com>
2024-10-01 14:46:02 +0700

    first commit

---
+++ backend/api/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .submodels.models_user import *
+# Register your models here.
+admin.site.register(Profile)
---
+++ backend/api/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+# Create your models here.
+from api.submodels.models_user import *
