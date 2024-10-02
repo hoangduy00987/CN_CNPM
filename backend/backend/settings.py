646679a09cd0725f48f3901e63c747ef17599ec8
khuongduy-hoang <hoangduy00987@gmail.com>
2024-10-02 13:55:34 +0700

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,13 +57,14 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+   'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware', 
 ]
 CORS_ALLOWED_ORIGINS = [
    'http://localhost:3000', 
