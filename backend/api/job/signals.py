db0be025e6643b7b5167f32d5d31f5d2ac8de91a
duongdinh2003 <lhdd1904@gmail.com>
2024-10-23 06:17:07 +0700

    update notifi

--- backend/api/job/signals.py
+++ backend/api/job/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
-from ..submodels.models_recruitment import Job
+from ..submodels.models_recruitment import Job, Application
 
 @receiver(post_save, sender=Job)
 def send_websocket_notification(sender, instance, **kwargs):
@@ -15,3 +15,15 @@ def send_websocket_notification(sender, instance, **kwargs):
         }
     )
     print('send to frontend')
+
+
+@receiver(post_save, sender=Application)
+def send_notification_when_new_apply(sender, instance, **kwargs):
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        'application_group',
+        {
+            'type': 'add_new_application',
+            'message': f'{instance.candidate.full_name} has just applied for {instance.job.title}. Please check it out.'
+        }
+    )
--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -13,7 +13,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 from channels.auth import AuthMiddlewareStack
-from api.job.consumers import JobConsumer
+from api.job.consumers import JobConsumer, ApplicationConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
@@ -22,6 +22,7 @@ application = ProtocolTypeRouter({
     "websocket": AuthMiddlewareStack(
         URLRouter([
             path('ws/updates/', JobConsumer.as_asgi()),  # Đường dẫn WebSocket
+            path('ws/new_application/', ApplicationConsumer.as_asgi()),
         ])
     ),
 })
