78389f20adccc6b94d5168b0eb4431e970746c3d
khuongduy-hoang <hoangduy00987@gmail.com>
2024-10-23 13:23:36 +0700

    test cicd

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,7 +16,7 @@ from datetime import timedelta
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # SECURITY WARNING: keep the secret key used in production secret!
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(dotenv_path=os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
